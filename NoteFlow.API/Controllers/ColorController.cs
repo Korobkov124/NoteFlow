6534a285a4b15db7a70686c20b388abf610d6ac9
Korobkov124 <andreyka.korobkov.97@mail.ru>
2026-03-27 00:26:42 +0700

    Added controller for color entity

--- NoteFlow.API/Controllers/ColorController.cs
+++ NoteFlow.API/Controllers/ColorController.cs
@@ -6,22 +6,23 @@ namespace NoteFlow.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class TagController : ControllerBase
+public class ColorController : ControllerBase
 {
-    private readonly TagService _service;
+    private readonly ColorService _service;
 
-    public TagController(TagService service)
+    public ColorController(ColorService service)
     {
         _service = service;
     }
 
-    [HttpGet("all")]
+    [HttpGet]
     [Authorize]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetAll()
     {
-        var response = await _service.GetAllTags();
+        var response = await _service.GetAll();
         return Ok(response);
     }
 }
--- NoteFlow.BLL/DependencyInjection.cs
+++ NoteFlow.BLL/DependencyInjection.cs
@@ -10,6 +10,7 @@ public static class DependencyInjection
         services.AddScoped<UsersService>();
         services.AddScoped<NoteService>();
         services.AddScoped<TagService>();
+        services.AddScoped<ColorService>();
         
         return services;
     }
